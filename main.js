88957a0b0db1eb31caa4731bed0e821f22621474
Sar2910 <90505917+Sar2910@users.noreply.github.com>
2023-05-25 16:10:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+var dog = 0;
+var lion = 0;
+var cat = 0;
+var cow = 0;
+
+function startclassification() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/D9oT5L13X/model.json', modelReady)
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random()*255) + 1;
+        random_number_g = Math.floor(Math.random()*255) + 1;
+        random_number_b = Math.floor(Math.random()*255) + 1;
+
+        document.getElementById("detected").innerHTML = "Detected Dog - "+ dog+ ", Detected Cat - "+ cat+ ", Detected Lion - "+ lion+ ", Detected Cow - "+ cow;
+        document.getElementById("detected").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+
+        document.getElementById("animal_voices").innerHTML = "Detected voice is of - " + results[0].label;
+        document.getElementById("animal_voices").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+
+        img = document.getElementById("animal_images");
+
+        if(results[0].label == "Barking") {
+            img.src = "https://shravaripatil.github.io/Sound_controlled_animals/bark.gif";
+            dog = dog + 1;
+        }
+        else if(results[0].label == "Mooing") {
+            img.src = "https://media.tenor.com/4mPjIpYolecAAAAM/cow-moo.gif";
+            cow = cow + 1;
+        }
+        else if(results[0].label == "Meowing") {
+            img.src = "https://shravaripatil.github.io/Sound_controlled_animals/meow.gif";
+            cat = cat + 1;
+        }
+        else if(results[0].label == "Roaring") {
+            img.src = "https://th.bing.com/th/id/R.ed424ce7e8aa8e8ea5909c5ec394be07?rik=Q5S23Xusd9rU%2bQ&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fKTj%2fXXX%2fKTjXXX8gc.gif&ehk=KUdjPcBBkT%2fjY0VxirZWUyUob6J0qb5M73sJqKYFXbA%3d&risl=&pid=ImgRaw&r=0";
+            lion = lion + 1;
+        }
+        else {
+            img.src = "https://shravaripatil.github.io/Sound_controlled_animals/listen.gif"
+        }
+    }
+}
